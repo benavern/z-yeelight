de74a068a8a4eb99cb472184df0f22e1061d8e5a
Benjamin Caradeuc <benjamin@caradeuc.info>
2019-10-17 00:40:28 +0200

    WIP redisign :  footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import Icon from './Icon'
 
 export default class Header extends Component {
   render() {
     return (
       <footer id="footer">
         <nav className="menu">
-          <NavLink to="/favorites">Favorites</NavLink>
+          <NavLink to="/favorites">
+            <div className="nav-icon">
+              <Icon name="heart" />
+            </div>
 
-          <NavLink to="/network">Discovered</NavLink>
+            <div className="nav-text">
+              Favorites
+            </div>
+          </NavLink>
+
+          <NavLink to="/discovered">
+            <div className="nav-icon">
+              <Icon name="question" />
+            </div>
+
+            <div className="nav-text">
+              Discovered
+            </div>
+          </NavLink>
         </nav>
       </footer>
     )
